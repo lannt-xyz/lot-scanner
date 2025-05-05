692e8885e3cee882983e47425feabbce92b15ac6
lanntxyz <lannt.xyz@gmail.com>
2025-05-05 19:38:56 +0700

    Add quick scan controller to FastAPI application

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from fastapi_pagination import add_pagination
 from app.controllers import (
     auth_controller,
     lot_scan_controller,
+    quick_scan_controller,
     secure_controller,
     config_controller,
     ticket_controller,
@@ -20,6 +21,7 @@ api_version_prefix = "/api/v1"
 app.include_router(lot_scan_controller.router, prefix=api_version_prefix, tags=["OCR"])
 app.include_router(config_controller.router, prefix=api_version_prefix, tags=["Configurations"])
 app.include_router(secure_controller.router, prefix=api_version_prefix, tags=["Secure"])
+app.include_router(quick_scan_controller.router, prefix=f'{api_version_prefix}/quick-scan', tags=["Quick Scan"])
 app.include_router(ticket_controller.router, prefix=f'{api_version_prefix}/tickets', tags=["Tickets"])
 
 # Add CORS middleware to the FastAPI application
