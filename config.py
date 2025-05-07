a6d833b1227a268726f83d67aae899f81aec68b5
lanntxyz <lannt.xyz@gmail.com>
2025-05-07 11:17:51 +0700

    Add scan limit settings for guests and users in configuration

--- config.py
+++ config.py
@@ -22,6 +22,10 @@ class Settings(BaseSettings):
     google_client_id: str
     google_client_secret: str
 
+    # Scan Limit Settings
+    scan_limit_guest: int = 0
+    scan_limit_user: int = 0
+
     # pylint: disable=too-few-public-methods
     class Config:
         env_file = Path(Path(__file__).resolve().parent) / ".env"
